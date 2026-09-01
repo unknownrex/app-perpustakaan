6ed0a991f99b739aba0f8d3ddd59f9b876e751c3
Rexample <reyvanfarrel@gmail.com>
2026-09-01 21:38:31 +0700

    [P-2] setup routing resource semua tabel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\MemberController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('books', BookController::class);
+Route::resource('categories', CategoryController::class)->except(['show']);
+Route::resource('members', MemberController::class);
+Route::resource('loans', LoanController::class);
+Route::put('/loans/{id}/kembalikan', [LoanController::class, 'kembalikan'])
+    ->name('loans.kembalikan');
